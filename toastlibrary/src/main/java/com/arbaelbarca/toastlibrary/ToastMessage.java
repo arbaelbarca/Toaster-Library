12deedd85d064984c74e532823744a7a9e3ed883
arbaelbarca <arba@clappinggape.com>
2020-08-05 15:38:44 +0700

    toast static

--- toastlibrary/src/main/java/com/arbaelbarca/toastlibrary/ToastMessage.java
+++ toastlibrary/src/main/java/com/arbaelbarca/toastlibrary/ToastMessage.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToastMessage {
-    public void toastMessage(Context context, String message) {
+    public static void toastMessage(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 }
